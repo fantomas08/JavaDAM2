5d44244a672a3e14171628ef2af277205cf1e678
fantomas08 <44437583+fantomas08@users.noreply.github.com>
2018-11-07 20:21:55 +0100

    dasas

--- eclipse-workspace/prueba/src/xmlDOM/Exercise3.java
+++ eclipse-workspace/prueba/src/xmlDOM/Exercise3.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
@@ -11,6 +12,9 @@ import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class Exercise3 implements Serializable {
 
@@ -78,17 +82,39 @@ public class Exercise3 implements Serializable {
 	}
 	
 	public static void createArray() {
-	
+		ArrayList<Person> al = new ArrayList<Person>();
+		File file = new File("PersonaXML.xml");
+		try {
+			Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(file);
+			NodeList list = doc.getElementsByTagName("person");
+			
+			for (int i = 0 ; i < list.getLength(); ++i) {
+				Node node = list.item(i);
+				
+				if (node.getNodeType() == Node.ELEMENT_NODE) {
+					Element element = (Element) node;
+					
+					al.add(new Person(element.getElementsByTagName("name").item(0).getTextContent(),
+							Integer.parseInt(element.getElementsByTagName("age").item(0).getTextContent())));
+				}
+			}
+			
+			System.out.println(al.toString());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public static void main(String[] args) {
 		ar = new Person[] {new Person("Yevgen", 27), new Person("Alex", 19), new Person("Enrique", 23),
 				new Person("Leandro", 25)};
 		//a
-		createFile();
+		//createFile();
 		//b
-		createXML();
-		
+		//createXML();
+		//c
+		createArray();
 	}
 
 }
